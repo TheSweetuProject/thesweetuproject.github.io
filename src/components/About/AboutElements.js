b2f47ed95df15b9e6e1c6dba02a3ce24db796e1a
Chhuong Le <chhuongle@gmail.com>
2021-11-23 16:41:34 -0800

    add dependencies and updated about component

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -3,11 +3,48 @@ import styled from 'styled-components';
 export const AboutContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-areas: "image about"
+  grid-template-areas: "image about";
   justify-contents: center;
   background-color: #F4E7D8;
+  font-family: Roboto;
 `;
 
 export const StyledImg = styled.img`
-  
+  max-width: 100%;
+`;
+
+export const AboutSection = styled.div`
+`;
+
+export const Header = styled.h1`
+  font-size: 50px;
+  margin-top: 120px;
+  font-weight: bold;
+`;
+
+export const BioSection = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: left;
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+export const BioSection2 = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: left;
+  font-size: 18px;
+  margin-top: 50px;
+`;
+
+export const Credentials = styled.div`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 13px;
+  margin-top: 50px;
+  margin-bottom: 50px;
 `;
