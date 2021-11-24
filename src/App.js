741059bfb9ffc636d7ac40655b317440efeff97a
Chhuong Le <chhuongle@gmail.com>
2021-11-23 17:40:53 -0800

    developed skeleton of the contact page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Landing from './components/Landing/Landing';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <About />
+      {/* <Landing /> */}
+      {/* <About /> */}
+      <Contact />
     </div>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,1 +1,55 @@
 import React from 'react';
+import {
+  ContactContainer,
+  FormContainer,
+  StyledButton
+} from './ContactElements.js';
+import { BsInstagram } from 'react-icons/bs';
+
+const style = {
+  resize: "none",
+}
+
+const Contact = () => {
+  return (
+    <ContactContainer>
+      <FormContainer>
+        <form>
+          <label style={{fontSize:"30px", marginRight:"70px"}}>Ask us Anything!</label> <br/>
+          <input
+            style={
+              {
+                fontFamily: "Roboto",
+                marginTop: "10px",
+                fontSize:"25px",
+                marginBottom:"10px"
+              }
+            }
+            size="20"
+            placeholder="Email"
+          />
+          <br/>
+          <textarea
+            style= {
+              {
+                resize:"none",
+                marginTop: "10px",
+                fontFamily: "Roboto",
+                marginBottom: "20px",
+                fontSize:"25px"
+              }
+            }
+            rows="5"
+            cols="22"
+            placeholder="Message"
+          />
+          <br/>
+          <StyledButton type="submit">Send</StyledButton>
+        </form>
+      </FormContainer>
+      <BsInstagram size={60}/>
+    </ContactContainer>
+  )
+}
+
+export default Contact;
