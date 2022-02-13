684cd6b44aff84090d7dfca17c4cf2e33b989d97
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-02-13 14:52:07 -0800

    instagram feature

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Landing.css';
+import InstagramFeed  from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
 
 const Landing = () => {
   return (
     <section>
-            <div className="hero-image">
-                <div className="hero-text">
-                    <h1>Care for a Slice?</h1>
-                    <p>For the Sweetest bite you'll ever taste</p>
-                    <p>Contact our prestigous baker to get started</p>
-                </div>
-            </div>
-            <div className="instagram">
-            </div>   
-        </section>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>Care for a Slice?</h1>
+          <p>For the Sweetest bite you'll ever taste</p>
+          <p>Contact our prestigous baker to get started</p>
+        </div>
+      </div>
+      <div className="instagram">
+        <InstagramFeed token={process.env.REACT_APP_INSTAGRAM_API_KEY}  counter="3"/> 
+      </div>   
+    </section>
   )
 }
 
